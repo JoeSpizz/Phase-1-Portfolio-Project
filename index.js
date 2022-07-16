1ee451225eb37e8590f5bc8b96eacc9ea2bc34c7
Joe <spizjo01@gmail.com>
2022-07-16 17:40:39 -0600

    hooked BoredAPI to activity form. conditioned responses.

--- index.js
+++ index.js
@@ -3,3 +3,57 @@ h1.addEventListener('click', alertf)
 function alertf(){
     alert("I've been clicked")
 }
+
+let actSubmit = document.querySelector("#activityForm")
+actSubmit.addEventListener('submit',activitySubmit)
+
+function activitySubmit(event){
+    event.preventDefault();
+   let chosenActivity = document.querySelector("#actSelect")
+   fetch(`http://www.boredapi.com/api/activity?type=${chosenActivity.value}`)
+   .then(res => res.json())
+  .then(event => {
+      let activityChosen = document.querySelector("#activityChosen");
+      let players = document.querySelector("#participants");
+      activityChosen.innerText = `${event.activity}`
+      players.innerText= `This event calls for ${event.participants}, but it's only a suggestion`
+      activityMessage(event)
+})
+}
+
+function activityMessage(Obj){
+    let typeMessage = document.querySelector("#actType")
+    if(Obj.type === 'recreational'){
+        typeMessage.innerText = `This recreational activity is about having fun! Indoors or out.`
+    }
+    else if (Obj.type === 'cooking'){
+        typeMessage.innerText = `Time to break out the apron and get cooking!`
+    }
+    else if (Obj.type === 'education'){
+        typeMessage.innerText = `I hope you've got your learning cap on. It's time for an education!`
+    }
+    else if (Obj.type === 'social'){
+        typeMessage.innerText = `Channel some extroverted energy and get out there! It's a social event!`
+    }
+    else if (Obj.type === 'music'){
+        typeMessage.innerText = `Will the music make you lose control? Only if you let it!`
+    }
+    else if (Obj.type === 'charity'){
+        typeMessage.innerText = `There has never been a better time to help others. It feels good to!`
+    }
+    else if (Obj.type === 'relaxation'){
+        typeMessage.innerText = `Unwind, take a deep breath, release. It is all about relaxation.`
+    }
+    else if (Obj.type === 'busywork'){
+        typeMessage.innerText = `Being productive is always a good option!`
+    }
+}
+
+
+
+                        //  cooking
+                        //  education
+                        //  social
+                        //  Music
+                        //  charity
+                        //  relaxation
